25c8a854345ec6531881599237c7eb3d0e2b7232
Darshit Kakadiya <darshit.kakadiya@shunyavkash.com>
2026-02-03 14:38:57 +0530

    Add execution time measurement to searchUserByName function

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,13 +4,18 @@ export const searchUserByName = async (req, res, next) => {
   try {
     const { name } = req.query;
 
+    const start = Date.now();
+
     const users = await User.find({
       name: new RegExp(name, "i"), // case-insensitive search
     });
 
+    const end = Date.now();
+
     res.status(200).json({
       success: true,
       data: users,
+      timeTakenMs: end - start,
     });
   } catch (error) {
     next(error);
